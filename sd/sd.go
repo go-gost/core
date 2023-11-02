55d7b2e3129ee09fefb929c288a19370bf83bdc9
ginuerzh <ginuerzh@gmail.com>
2023-11-02 20:50:25 +0800

    update sd interface

--- sd/sd.go
+++ sd/sd.go
@@ -9,16 +9,17 @@ type Options struct{}
 type Option func(opts *Options)
 
 type Service struct {
-	Node    string
+	ID      string
 	Name    string
+	Node    string
 	Network string
 	Address string
 }
 
 // SD is the service discovery interface.
 type SD interface {
-	Register(ctx context.Context, name string, network string, address string, opts ...Option) error
-	Deregister(ctx context.Context, name string) error
-	Renew(ctx context.Context, name string) error
+	Register(ctx context.Context, service *Service, opts ...Option) error
+	Deregister(ctx context.Context, service *Service) error
+	Renew(ctx context.Context, service *Service) error
 	Get(ctx context.Context, name string) ([]*Service, error)
 }
